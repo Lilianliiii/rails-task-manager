fe0673e26cc4945927d19f92c5ca65f93f59541d
Lilian Li <lil712@hotmail.com>
2022-11-16 22:11:02 +0800

    refactored code on controller

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,38 @@
+class TasksController < ApplicationController
+  def all
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task.update(task_params)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:name, :details, :completed)
+  end
+end
